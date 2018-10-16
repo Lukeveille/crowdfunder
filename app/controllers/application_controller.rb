012b7bb9acbb0a0e9b82b39c695f09a31dffc110
kevintudiep <kevintudiep@gmail.com>
2018-10-16 08:50:56 -0500

    added positive dollar pledge test, also added error messages to login, pledge and sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   def require_login
     unless current_user
       flash[:alert] = "Please log in"
-      redirect_to new_session_url
+      redirect_to login_url
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 8 }, on: :create
   validates :password, confirmation: true, on: :create
   validates :password_confirmation, presence: true, on: :create
+  validates :email, presence: true
 
   validates :email, uniqueness: true
 end
