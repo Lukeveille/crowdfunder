c9cc4789f5fee52c39b786b2e541d4d7178414b8
kevintudiep <kevintudiep@gmail.com>
2018-10-17 16:30:47 -0500

    fixed creating project error, adjusted project validation where start time, end time and time now cannot equal to nil

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,13 +11,13 @@ class Project < ActiveRecord::Base
   validate :end_date_if_cannot_be_before_start_date, :start_date_cannot_be_in_past
 
   def end_date_if_cannot_be_before_start_date
-    if end_date < start_date
+    if start_date != nil && end_date != nil && end_date < start_date
       errors.add(:end_date, "Must be later than start date.")
     end
   end
 
   def start_date_cannot_be_in_past
-    if start_date < Time.now
+    if start_date != nil && Time.now != nil && start_date < Time.now
       errors.add(:start_date, "Start date cannot be in the past.")
     end
   end
