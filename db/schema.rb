0a5d92d05c9c7d2a402167e12248dce98b912f3b
Lukeveille <luke@lukeleveille.com>
2018-10-18 12:19:06 -0400

    comments exist, have correct associations, and a functioning model test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_202351) do
+ActiveRecord::Schema.define(version: 2018_10_18_155815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pledges", id: :serial, force: :cascade do |t|
     t.integer "user_id"
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_comment_needs_text
+    comment = Comment.new(comment: '', project_id: Project.first)
+    assert comment.invalid?, "Comments should not be able to be created without text in the body"
+  end
 end
